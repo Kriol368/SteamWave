8027e16e0cd9907a86ad7413507027b2ac8b3f9c
Kriol368 <kriol368@gmail.com>
2024-10-31 10:03:05 +0100

    added field name to user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -32,6 +32,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
+    #[ORM\Column(length: 255)]
+    private ?string $NAME = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -110,6 +113,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setIsVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getNAME(): ?string
+    {
+        return $this->NAME;
+    }
+
+    public function setNAME(string $NAME): static
+    {
+        $this->NAME = $NAME;
 
         return $this;
     }
