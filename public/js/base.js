e65180c4791293655630103e165920cbefe1512f
alumno (mv reventada de clase) <mauricio@quesoyyocoo.com>
2024-11-05 10:40:04 +0100

    añadido reset, y js para el base

---
+++ public/js/base.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+}
