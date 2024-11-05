f88567af5d5e3f1c1a8539ae45ef55b2b5196370
Kriol368 <kriol368@gmail.com>
2024-11-05 11:38:02 +0100

    added the apikey as a secret

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -129,7 +129,7 @@ class RegistrationController extends AbstractController
 
     private function resolveVanityURLToSteamID(string $vanityName): ?string
     {
-        $steamApiKey = '562D15EBA45FEC235C627957E91296DE';
+        $steamApiKey = $this->getParameter('steam.api_key');
         $response = $this->httpClient->request('GET', "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key={$steamApiKey}&vanityurl={$vanityName}");
         $data = $response->toArray();
 
