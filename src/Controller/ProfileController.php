bb8be596a99bd3f4d0930dd3c841328d4afa11eb
Marc Ibàñez <mibanezp0709@gmail.com>
2024-11-13 13:49:54 +0100

    proceso del profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function index(UserRepository $userRepository): Response
     {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Debe estar autenticado para ver esta página.');
+        }
+
+        // En este caso, asumimos que $user ya es la entidad `User`
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user,
         ]);
     }
 }
