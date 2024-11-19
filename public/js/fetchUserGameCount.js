534bf6ee97443af79213fd2a975148a7355142f1
MaurisioDTS <96304957+MaurisioDTS@users.noreply.github.com>
2024-11-19 11:50:00 +0100

    nos dhneghy nmruev

---
+++ public/js/fetchUserGameCount.js
@@ -0,0 +1,16 @@
+// public/js/fetchUserGameCount.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/user/games-list')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const gamesList = document.getElementById('user_games_list');
+
+        })
+        .catch(error => console.error('There was a problem with the fetch operation:', error));
+});
