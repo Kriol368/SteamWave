41c717a71cbf827521041a0bf940d2c9f484f972
Kriol368 <kriol368@gmail.com>
2024-12-09 09:49:34 +0100

    reviews implementadas correctamente

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -1,8 +1,13 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Review;
+use App\Form\ReviewFormType;
+use App\Repository\ReviewRepository;
 use App\Service\SteamAppService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,10 +21,27 @@ class GameController extends AbstractController
     }
 
     #[Route('/game/{appId}', name: 'app_game')]
-    public function index(int $appId): Response
+    public function index(int $appId, Request $request, ReviewRepository $reviewRepository, EntityManagerInterface $em): Response
     {
         // Fetch detailed game information
         $gameDetails = $this->steamAppService->getGameDetails($appId);
+
+        $reviews = $reviewRepository->findBy(['game' => (string) $appId], ['publishedAt' => 'DESC']);
+
+        $review = new Review();
+        $form = $this->createForm(ReviewFormType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $review->setGame((string) $appId); // Use the appId to link the review to the game
+            $review->setUser($this->getUser()); // Attach the logged-in user
+            $review->setPublishedAt(new \DateTimeImmutable()); // Set the current date/time
+            $em->persist($review);
+            $em->flush();
+
+            // Redirect to prevent resubmission of the form
+            return $this->redirectToRoute('app_game', ['appId' => $appId]);
+        }
 
         if (!$gameDetails) {
             throw $this->createNotFoundException("Game with ID $appId not found.");
@@ -27,6 +49,8 @@ class GameController extends AbstractController
 
         return $this->render('game/index.html.twig', [
             'game' => $gameDetails,
+            'reviews' => $reviews,
+            'reviewForm' => $form->createView(),
         ]);
     }
 }
