901c5aa0ad54f1d342ab763117982107f2da51bc
MaurisioDTS <96304957+MaurisioDTS@users.noreply.github.com>
2024-12-12 10:39:18 +0100

    cooking el controlador

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MessageController extends AbstractController
 {
-    #[Route('/message', name: 'app_message')]
-    public function index(): Response
+    private $entityManager=doc;
+
+    #[Route('/send/message', name: 'app_message', methods: ['POST'])]
+    public function sendMessage(Request $request): JsonResponse
     {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
-        ]);
+        $data = json_decode($request->getContent(), true); // pillas el json
+        $message = $data['message'] ?? null;    // le dices que dentro del array que saque la propiedad 'message' o sino que sea null. en este caso la única que hay.
+
+        if ($message) {
+            $chat = $this->entityManager->getRepository(Product::class)->find($id);
+            new Message($message, $this->getUser(), $chat);
+
+            return new JsonResponse(['status' => 'success', 'message' => $message]);
+        } else {
+            return new JsonResponse(['status' => 'error', 'message' => 'No message provided'], 400);
+        }
     }
 }
