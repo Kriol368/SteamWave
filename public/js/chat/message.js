503c1bbc2af6b67cb204cf9056952c4670da0696
Marc Ibàñez <148450674+Marpuchy@users.noreply.github.com>
2025-01-07 12:05:59 +0100

    Update message.js

--- public/js/chat/message.js
+++ public/js/chat/message.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+    function sendMessage() {
+        var message = $('#message').val();
+        var chatId = 1; // Asegúrate de obtener el ID del chat correctamente
+
+        if (message.trim() === '') {
+            alert('Por favor, escribe un mensaje.');
+            return;
+        }
+
+        $.ajax({
+            url: '/send/message',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ message: message, chat: chatId }), // Enviar el ID del chat
+            success: function(response) {
+                if (response.status === 'success') {
+                    $('#response').text('Mensaje enviado: ' + response.message);
+                    $('#message').val(''); // Limpiar el campo de entrada
+                    scrollToBottom(); // Desplazar hacia abajo después de enviar el mensaje
+                } else {
+                    $('#response').text('Error: ' + response.message);
+                }
+            },
+            error: function(xhr, status, error) {
+                $('#response').text('Error: ' + error);
+            }
+        });
+    }
     $('#sendMessageButton').click(function() {
         var url = $(location).attr('href'),
             parts = url.split("/"),
